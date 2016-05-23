a811abd7bb6ecfa371573b7ce03eb42671b0d661
Arel Gindos <rlgindos@gmail.com>
2016-05-23 20:37:55 +0300

    added insert for sitters/parents/invites/users

--- index.js
+++ index.js
@@ -3,111 +3,95 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
     fs = require('fs'),
+    bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    db = mongoose.connect('mongodb://db_usr:db_pass@ds011913.mlab.com:11913/sitters'),
     Parent = require('./parent'),
     Sitter = require('./sitter'),
     Invites = require('./invites'),
     Sitters = require('./sitters_modules'),
     Users = require('./user'),
+    db,
     SittersData = null,tempJson = null,
     tempParents,tempSitters,tempInvites,tempUsers;
 
 
-mongoose.connection.once('open', function(){
-    Invites.find({}, function(err,sittersData){
-        console.log("connection established to mongoDB\nyou can now use the functions");
-        tempInvites = sittersData;
-        SittersData = new Sitters(sittersData); // get all the json from mongoDB and send it to constructor
-       // mongoose.disconnect();
+mongoose.connect('mongodb://db_usr:db_pass@ds011913.mlab.com:11913/sitters');
+db = mongoose.connection;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true}));
 
+
+db.once('connected', function(){
+    console.log('getting data');
+    Invites.find({}, function(err,sittersData){
+        tempInvites = sittersData;
     });
 
     Parent.find({}, function(err,sittersData){
-        console.log("connection established to mongoDB\nyou can now use the functions");
         tempParents = sittersData;
-       // SittersData = new Sitters(sittersData); // get all the json from mongoDB and send it to constructor
-       // mongoose.disconnect();
-
     });
-    Users.find({}, function(err,sittersData){
-        console.log("connection established to mongoDB\nyou can now use the functions");
-        tempUsers = sittersData;
 
+    Users.find({}, function(err,sittersData){
+        tempUsers = sittersData;
     });
 
     Sitter.find({}, function(err,sittersData){
-        console.log("connection established to mongoDB\nyou can now use the functions");
         tempSitters = sittersData;
         SittersData = new Sitters(tempParents,tempSitters,tempInvites,tempUsers); // get all the json from mongoDB and send it to constructor
-        mongoose.disconnect();
-
+       // mongoose.disconnect(); // TODO: need to think of when to close connection
     });
-    //console.log(tempInvites);
-    // console.log(tempParents);
-    //console.log(tempSitters);
 });
 
-/*
-mongoose.connection.once('open', function(){
-    Parent.find({}, function(err,dataTemp1){
-        tempParents = dataTemp1;
-        console.log(dataTemp1);
+app.post('/insertUser' ,function(req,res){ //TODO:  send json in react
+    tempJson = new Users(req.body);
+    tempJson.save(function(err , doc){
+       if(err)
+           console.log(err);// TODO: take care of error
+        else
+           console.log(req.body);
+        res.status(200).json(req.body); // just for debugging
     });
-   /* Sitter.find({}, function(err,dataTemp2){
-        tempSitters = dataTemp2;
-    });
-    Invites.find({}, function(err,dataTemp3){
-        tempInvites = dataTemp3;
-    });
-    console.log(tempParents);
-    SittersData = new Sitters(tempParents,tempSitters,tempInvites); // get all the json from mongoDB and send it to constructor
-    mongoose.disconnect();
-});*/
-
-app.get('/getAllData', function(req,res){
-    res.status(200).json(SittersData.getAllStudentsGrades());
 });
 
-app.get('/getStudGradeById/:id', function(req,res){
-    tempJson = SittersData.getStudGradeById(req.params.id);
-    if (tempJson.status == false){
-        res.set('header-getStudGradeById',"wrong id input");
-        res.status(400).json(tempJson);
-    }
-    else
-        res.status(200).json(tempJson);
+app.post('/insertSitter' ,function(req,res){ //TODO:  send json in react
+    tempJson = new Sitter(req.body);
+    tempJson.save(function(err , doc){
+        if(err)
+            console.log(err);// TODO: take care of error
+        else
+            console.log(req.body);
+        res.status(200).json(req.body); // just for debugging
+    });
 });
 
-app.get('', function(req,res){
-    tempJson = SittersData.getExcellenceByYear(req.params.year);
-    if (tempJson.status == false){
-        res.set('header-getExcellenceByYear',"cant find any studnet in the year");
-        res.status(400).json(tempJson);
-    }
-    else
-        res.status(200).json(tempJson);
+app.post('/insertParent' ,function(req,res){ //TODO: send json in react
+    tempJson = new Parent(req.body);
+    tempJson.save(function(err , doc){
+        if(err)
+            console.log(err);// TODO: take care of error
+        else
+            console.log(req.body);
+        res.status(200).json(req.body); // just for debugging
+    });
 });
 
-app.get('', function(req,res){
-    tempJson = SittersData.getWorstAverageByYear(req.params.year);
-    if (tempJson.status == false){
-        res.set('header-getWorstAverageByYear',"cant find any studnet in the year");
-        res.status(400).json(tempJson);
-    }
-    else
-        res.status(200).json(tempJson);
+app.post('/insertInvite' ,function(req,res){ //TODO:  send json in react
+    tempJson = new Invites(req.body);
+    tempJson.save(function(err , doc){
+        if(err)
+            console.log(err);// TODO: take care of error
+        else
+            console.log(req.body);
+        res.status(200).json(req.body); // just for debugging
+    });
 });
 
 app.get('/', function(req,res){
-    res.status(400).json({'Error' : 'oops... you pressed wrong path, please look at the api',
-        'api': ''});
+    res.status(400).json({'Error' : 'oops...  wrong path'});
 });
 
-
 app.all('*', function(req,res){
-    res.status(400).json({'Error' : 'oops... you pressed wrong path, please look at the api',
-        'api': ''});
+    res.status(400).json({'Error' : 'oops...  wrong path'});
 });
 
 http.createServer(app).listen(port);
