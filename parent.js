801effeb519cf33e2c7698b6ebb46011114ad645
ArelGindos <arel-g@hotmail.com>
2016-07-07 17:07:12 +0300

    fixed headers and parent schema

--- parent.js
+++ parent.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
 
 var schema_name = new schema({
     email : {type : String, unique : true , required : true},
-    password : {type : String, required : true},
+    password : {type : String},
     name : String,
     profilePictureURL : String,
     fullPictureURL : String,
